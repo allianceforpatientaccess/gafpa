f50c8a89899f46c953ce46a5e912d4b7d8cbdf2e
Avi Nerenberg <devavmeister@gmail.com>
2018-08-31 14:56:54 -0400

    updating resources page

--- wp-content/themes/gafpa/templates/resources.php
+++ wp-content/themes/gafpa/templates/resources.php
@@ -22,41 +22,47 @@
    /* RESOURCES */
 
    // TAXONOMY ARRAYS
+   $region_queries = array(
+                        'europe',
+                        'latin-america',
+                        'international'
+                     );
+
+   $media_queries = array(
+                        'poster',
+                        'report',
+                        'fast-facts',
+                        'policy-papers',
+                        'infographics'
+                     );
+
    // the static part of the taxonomy queries
    $tax_query_slug                  =  array(
                                           'taxonomy'  => 'attachment_tag',
                                           'field'     => 'slug',
                                           'operator'  => 'AND',
                                        );
-   // the static "resources" query
-   $resources_tax_query             =  array(
-                                          'taxonomy'  => 'attachment_category',
-                                          'field'     => 'slug',
-                                          'terms'     => 'resources',
-                                       );
 
-   // Europe tax arrays
-   $europe_posters_tax_query        =  array_merge( $tax_query_slug, array( 'terms' => array( 'europe', 'poster' ) ) );
-   $europe_reports_tax_query        =  array_merge( $tax_query_slug, array( 'terms' => array( 'europe', 'report' ) ) );
-   $europe_fastfacts_tax_query      =  array_merge( $tax_query_slug, array( 'terms' => array( 'europe', 'fast-facts' ) ) );
-   $europe_policypapers_tax_query   =  array_merge( $tax_query_slug, array( 'terms' => array( 'europe', 'policy-papers' ) ) );
-   $europe_infographics_tax_query   =  array_merge( $tax_query_slug, array( 'terms' => array( 'europe', 'infographics' ) ) );
+   $tax_arrays          = array();
+   $regionQueriesCount  = 0;
 
-   // Latin America tax arrays
-   $latin_america_posters_tax_query        =  array_merge( $tax_query_slug, array( 'terms' => array( 'latin-america', 'poster' ) ) );
-   $latin_america_reports_tax_query        =  array_merge( $tax_query_slug, array( 'terms' => array( 'latin-america', 'report' ) ) );
-   $latin_america_fastfacts_tax_query      =  array_merge( $tax_query_slug, array( 'terms' => array( 'latin-america', 'fast-facts' ) ) );
-   $latin_america_policypapers_tax_query   =  array_merge( $tax_query_slug, array( 'terms' => array( 'latin-america', 'policy-papers' ) ) );
-   $latin_america_infographics_tax_query   =  array_merge( $tax_query_slug, array( 'terms' => array( 'latin-america', 'infographics' ) ) );
+   // generates an array of the specific taxonomy for each query
+   for ($count = 0; $count <= 15; $count++) {
+      array_push( $tax_arrays, array_merge( $tax_query_slug, array( 'terms' => array( $region_queries[$regionQueriesCount], $media_queries[$count % 5] ) ) ) );
 
-   // International tax arrays
-   $international_posters_tax_query        =  array_merge( $tax_query_slug, array( 'terms' => array( 'international', 'poster' ) ) );
-   $international_reports_tax_query        =  array_merge( $tax_query_slug, array( 'terms' => array( 'international', 'report' ) ) );
-   $international_fastfacts_tax_query      =  array_merge( $tax_query_slug, array( 'terms' => array( 'international', 'fast-facts' ) ) );
-   $international_policypapers_tax_query   =  array_merge( $tax_query_slug, array( 'terms' => array( 'international', 'policy-papers' ) ) );
-   $international_infographics_tax_query   =  array_merge( $tax_query_slug, array( 'terms' => array( 'international', 'infographics' ) ) );
+      if($count % 5 == 4) {
+         $regionQueriesCount++;  // only increment every 5 cycles (to cycle through each media type for each region)
+      }
+   }
 
    // ARGUMENT ARRAYS
+   // the static "resources" query
+   $resources_tax_query =  array(
+                           'taxonomy'  => 'attachment_category',
+                           'field'     => 'slug',
+                           'terms'     => 'resources',
+                        );
+
    // the static part of the WP_Query argument arrays
    $arg_array_slug = array(
                         'post_type'       => 'attachment',  // media queries
@@ -64,63 +70,20 @@
                         'post_status'     => 'inherit',     // WP Query defaults to post_status = 'default', whereas attachments are 'inherit'
                      );
 
-   // Europe arg arrays
-   $europe_posters      = array_merge( $arg_array_slug, array( 'tax_query' => array( $resources_tax_query, $europe_posters_tax_query ) ) );
-   $europe_reports      = array_merge( $arg_array_slug, array( 'tax_query' => array( $resources_tax_query, $europe_reports_tax_query ) ) );
-   $europe_fastfacts    = array_merge( $arg_array_slug, array( 'tax_query' => array( $resources_tax_query, $europe_fastfacts_tax_query ) ) );
-   $europe_policypapers = array_merge( $arg_array_slug, array( 'tax_query' => array( $resources_tax_query, $europe_policypapers_tax_query ) ) );
-   $europe_infographics = array_merge( $arg_array_slug, array( 'tax_query' => array( $resources_tax_query, $europe_infographics_tax_query ) ) );
+   $arg_arrays = array();
+   $queries    = array();
 
-   // Latin America arg arrays
-   $latin_america_posters      = array_merge( $arg_array_slug, array( 'tax_query' => array( $resources_tax_query, $latin_america_posters_tax_query ) ) );
-   $latin_america_reports      = array_merge( $arg_array_slug, array( 'tax_query' => array( $resources_tax_query, $latin_america_reports_tax_query ) ) );
-   $latin_america_fastfacts    = array_merge( $arg_array_slug, array( 'tax_query' => array( $resources_tax_query, $latin_america_fastfacts_tax_query ) ) );
-   $latin_america_policypapers = array_merge( $arg_array_slug, array( 'tax_query' => array( $resources_tax_query, $latin_america_policypapers_tax_query ) ) );
-   $latin_america_infographics = array_merge( $arg_array_slug, array( 'tax_query' => array( $resources_tax_query, $latin_america_infographics_tax_query ) ) );
+   // generates an array of complete argument arrays for each query
+   foreach ($tax_arrays as $tax_array) {
+      array_push( $arg_arrays, array_merge( $arg_array_slug, array( 'tax_query' => array( $resources_tax_query, $tax_array ) ) ) );
+   }
 
-   // International arg arrays
-   $international_posters      = array_merge( $arg_array_slug, array( 'tax_query' => array( $resources_tax_query, $international_posters_tax_query ) ) );
-   $international_reports      = array_merge( $arg_array_slug, array( 'tax_query' => array( $resources_tax_query, $international_reports_tax_query ) ) );
-   $international_fastfacts    = array_merge( $arg_array_slug, array( 'tax_query' => array( $resources_tax_query, $international_fastfacts_tax_query ) ) );
-   $international_policypapers = array_merge( $arg_array_slug, array( 'tax_query' => array( $resources_tax_query, $international_policypapers_tax_query ) ) );
-   $international_infographics = array_merge( $arg_array_slug, array( 'tax_query' => array( $resources_tax_query, $international_infographics_tax_query ) ) );
+   // generates an array of the WP_Query objects
+   foreach ($arg_arrays as $args) {
+      array_push( $queries, new WP_Query( $args ) );
+   }
 
-   // QUERY OBJECTS
-   $europe_posters_query = new WP_Query($europe_posters);
-   $europe_reports_query = new WP_Query($europe_reports);
-   $europe_fastfacts_query = new WP_Query($europe_fastfacts);
-   $europe_policypapers_query = new WP_Query($europe_policypapers);
-   $europe_infographics_query = new WP_Query($europe_infographics);
-
-   $latin_america_posters_query = new WP_Query($latin_america_posters);
-   $latin_america_reports_query = new WP_Query($latin_america_reports);
-   $latin_america_fastfacts_query = new WP_Query($latin_america_fastfacts);
-   $latin_america_policypapers_query = new WP_Query($latin_america_policypapers);
-   $latin_america_infographics_query = new WP_Query($latin_america_infographics);
-
-   $international_posters_query = new WP_Query($international_posters);
-   $international_reports_query = new WP_Query($international_reports);
-   $international_fastfacts_query = new WP_Query($international_fastfacts);
-   $international_policypapers_query = new WP_Query($international_policypapers);
-   $international_infographics_query = new WP_Query($international_infographics);
-
-   $queries = array(
-      $europe_posters_query,
-      $europe_reports_query,
-      $europe_fastfacts_query,
-      $europe_policypapers_query,
-      $europe_infographics_query,
-      $latin_america_posters_query,
-      $latin_america_reports_query,
-      $latin_america_fastfacts_query,
-      $latin_america_policypapers_query,
-      $latin_america_infographics_query,
-      $international_posters_query,
-      $international_reports_query,
-      $international_fastfacts_query,
-      $international_policypapers_query,
-      $international_infographics_query,
-   );
+   /* PRINTING */
 
    $region = array(
       "Europe",
@@ -137,30 +100,32 @@
    );
 
    $count = 0;       // print the corresponding header text for each query
-   $regionCount = 0; // print the region every 5 cycles (1 cycle of the media array)
+   $regionCount = 0; // print the region every 5 cycles (1 cycle of the media array) ?>
 
-   foreach ($queries as $query) :
+   <section style="background: white;">
+      <?php foreach ($queries as $query) :
 
-      if ($count % 5 == 0) : ?>
-         <h2 class="region"><?php echo $region[$regionCount]; ?></h2>
-         <?php $regionCount++;
-      endif;
-      
-      if ($query->have_posts()) : ?>
-         <h3><?php echo $media[$count % 5]; ?></h3>
-      <?php endif;
+         if ($count % 5 == 0) : ?>
+            <h2 style="text-align: center;"><?php echo $region[$regionCount]; ?></h2>
+            <?php $regionCount++;
+         endif;
+         
+         if ($query->have_posts()) : ?>
+            <h3 style="text-align: center;"><?php echo $media[$count % 5]; ?></h3>
+         <?php endif;
 
-      $count++;
+         $count++;
 
-      while ($query->have_posts()) :
-         $query->the_post() ?>
-         <p><a style="text-align: center;" href="<?php echo the_permalink() ?>"><?php echo the_title() ?></a></p>
-      <?php endwhile;
-   endforeach;
+         while ($query->have_posts()) :
+            $query->the_post() ?>
+            <p style="text-align: center;"><a href="<?php echo the_permalink() ?>"><?php echo the_title() ?></a></p>
+         <?php endwhile;
 
-   wp_reset_postdata();
+         wp_reset_postdata();
+      endforeach; ?>
+   </section>
 
-   /* STATIC CONTENT */
+   <?php /* STATIC CONTENT */
    if (strpos(get_permalink($post->ID), 'event') !== false) :
       get_template_part('templates/partials/sc', 'event-list');
    endif;
