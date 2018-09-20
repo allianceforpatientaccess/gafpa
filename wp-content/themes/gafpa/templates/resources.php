a682da49acc1eeb9f11b6ed91372c58e266d330a
Avi Nerenberg <devavmeister@gmail.com>
2018-09-19 21:11:33 -0400

    patching image/pdf display for resources

--- wp-content/themes/gafpa/templates/resources.php
+++ wp-content/themes/gafpa/templates/resources.php
@@ -124,13 +124,12 @@
 
          // print post (The Loop)
          while ($query->have_posts()) :
-            $query->the_post();
+            /*$query->the_post();
             $image = wp_get_attachment_url();                        // URL of the image (work around for lack of thumbnail)
-            $imageData = base64_encode(file_get_contents($image));   // the encoded image ?>
+            $imageData = base64_encode(file_get_contents($image));*/   // the encoded image ?>
 
-            <p style="text-align: center;"><a style="color: #142945;" href="<?php echo the_permalink() ?>"><?php echo '<img style="height: 300px;" src="data:image/jpeg;base64,'.$imageData.'">' ?></a></p>
+            <p style="text-align: center;"><a style="color: #142945;" href="<?php echo the_permalink() ?>"><!--?php echo '<img style="height: 300px;" src="data:image/jpeg;base64,'.$imageData.'">' ?--></a></p>
             <p style="text-align: center;"><a style="color: #142945;" href="<?php echo the_permalink() ?>"><?php echo the_title() ?></a></p>
-
          <?php endwhile;
 
          wp_reset_postdata();
