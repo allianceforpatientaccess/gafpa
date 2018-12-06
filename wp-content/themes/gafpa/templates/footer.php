a43ce1bbcb1c0f62665e3f746414ed54330712c5
Avi Nerenberg <devavmeister@gmail.com>
2018-12-06 00:27:27 -0500

    adding registration form to footer

--- wp-content/themes/gafpa/templates/footer.php
+++ wp-content/themes/gafpa/templates/footer.php
@@ -13,12 +13,9 @@
 ?>
 
 <!-- subscribe button -->
-<!--?php if(isset($isSubscribe)) : // TODO: check to see if it's the subscribe page? -->
-   <section style="text-align: center; padding: 100px 0;">
-      <a id="subscribe" style="text-transform: uppercase; color: white;" href="<?php echo get_site_url(); ?>/email-signup/">Subscribe</a>
-			<?php gravity_form( 8, $display_title = false, $display_description = false ); ?>
-   </section>
-<!--?php endif; ?-->
+<section id="subscribe-form" style="text-align: center; padding: 100px 0; display: flex; flex-direction: column; align-items: center;">
+	<?php gravity_form( 8, $display_title = false, $display_description = false ); ?>
+</section>
 <!-- /subscribe button -->
 
 <footer class="container-fluid">
