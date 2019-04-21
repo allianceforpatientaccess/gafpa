8d8bb357bc39e33df17c4bc1064ef781b10633c3
Avi Nerenberg <devavmeister@gmail.com>
2019-04-21 19:28:04 -0400

    adding EAfPA logo in navbar

--- wp-content/themes/gafpa/templates/europe.php
+++ wp-content/themes/gafpa/templates/europe.php
@@ -1,4 +1,16 @@
-<?php /*Template Name: Europe*/
-	set_query_var( 'region', 'europe' ); // pass this variable & value to the called template part
+<?php /*Template Name: Europe*/ ?>
+	<!-- EAfPA logo -->
+	<style>
+		.navbar-brand > img:nth-child(1) {
+			display: block;
+  		box-sizing: border-box;
+
+			background: url(http://gafpa.staging.wpengine.com/wp-content/uploads/EAfPA-logo.png) no-repeat;
+			background-size: 100px;
+			padding-left: 100px;
+		}
+	</style>
+
+	<?php set_query_var( 'region', 'europe' ); // pass this variable & value to the called template part
 	get_template_part( 'templates/resources' ); 
 ?>
