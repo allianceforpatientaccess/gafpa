d8ab743259d5f53e23d39bed73b20f91086e14a6
Avi Nerenberg <devavmeister@gmail.com>
2019-04-21 20:06:35 -0400

    updates to eafpa replacement img

--- wp-content/themes/gafpa/templates/europe.php
+++ wp-content/themes/gafpa/templates/europe.php
@@ -1,16 +1,22 @@
 <?php /*Template Name: Europe*/ ?>
-	<!-- EAfPA logo -->
-	<style>
-		.navbar-brand > img:nth-child(1) {
-			display: block;
-  		box-sizing: border-box;
 
-			background: url(http://gafpa.staging.wpengine.com/wp-content/uploads/EAfPA-logo.png) no-repeat;
-			background-size: 100px;
-			padding-left: 100px;
-		}
-	</style>
+<!-- EAfPA logo -->
+<style>
+	.navbar-brand {
+		height: 150px;
+		width: 150px;
 
-	<?php set_query_var( 'region', 'europe' ); // pass this variable & value to the called template part
-	get_template_part( 'templates/resources' ); 
-?>
+		display: block;
+		background: url(http://gafpa.staging.wpengine.com/wp-content/uploads/EAfPA-logo.png) no-repeat;
+		background-size: 150px;
+		overflow: hidden;
+	}
+
+	.navbar-brand > img:nth-child(1) {
+		margin-left: -150px;
+	}
+</style>
+<!-- /EAfPA logo -->
+
+<?php set_query_var( 'region', 'europe' ); // pass this variable & value to the called template part
+get_template_part( 'templates/resources' ); ?>
